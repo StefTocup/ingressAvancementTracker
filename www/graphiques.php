d850068999887f61fdeb2a83a251dbed17f52eda
Stef Tocup <groumph@gmail.com>
2014-09-10 15:00:04 +0200

    Commits de pur forme

---
+++ www/graphiques.php
@@ -0,0 +1,12 @@
+<?php
+	require_once("entete.php");
+	require_once("MySQLConnect.php");
+?>
+<h2>Travail en cours</h2>
+<h3>H&acirctez-vous lentement, et, sans perdre courage,<br/>
+Vingt fois sur le m&eacute;tier remettez votre ouvrage :<br/>
+Polissez-le sans cesse et le repolissez ;<br/>
+Ajoutez quelquefois, et souvent effacez</h3>
+<?php
+	require_once("enqueue.php");
+?>
